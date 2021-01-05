d155ad28c3008bec85837666b68e2c1a6fb8e760
irinaeris <72436931+irinaeris@users.noreply.github.com>
2021-01-05 23:18:56 +0200

    Add files via upload

---
+++ Hooks.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema_28._10
+{
+    public class Hooks
+    {
+        protected IWebDriver Driver; 
+
+        [SetUp]
+        public void Setup() 
+        {
+            Driver = new ChromeDriver(); 
+            Driver.Manage().Window.Maximize();
+            Driver.Navigate().GoToUrl("http://google.com");
+
+        }
+    }
+}
---
+++ Tests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Tema_28._10.PageObjects;
+using OpenQA.Selenium;
+
+namespace Tema_28._10
+{
+    [TestFixture]
+    class Tests : Hooks 
+    {
+        [Test]
+        public void GoogleImagesSearch()
+        {
+            //Arrange
+            HomePage homePage = new HomePage(Driver);
+
+            // Act
+            Driver.SwitchTo().Frame(0);
+            homePage.GoToAgreeButton();
+            System.Threading.Thread.Sleep(2000);
+            homePage.SearchImage("paris");
+            System.Threading.Thread.Sleep(2000);
+            homePage.SelectImage();
+            System.Threading.Thread.Sleep(2000);
+            Driver.Navigate().Back();
+
+            //Assert
+            Assert.IsTrue(homePage.searchTextBox.Displayed);
+        }
+
+    }
+}
+
