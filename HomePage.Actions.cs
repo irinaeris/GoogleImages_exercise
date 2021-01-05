b353245f166d3ca79cf3b306db3310c524326b92
irinaeris <72436931+irinaeris@users.noreply.github.com>
2021-01-05 23:18:12 +0200

    Add files via upload

---
+++ HomePage.Actions.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tema_28._10.PageObjects
+{
+    partial class HomePage
+    {
+        public void GoToAgreeButton()
+        {
+            agreeButton.Click();
+        }
+
+        public void SearchImage(string query)
+        {
+            searchTextBox.SendKeys(query);
+            searchTextBox.SendKeys(Keys.Enter);
+        }
+
+        public void SelectImage()
+        {
+            imagesButton.Click();
+            firstImageFound.Click();
+        }
+    }
+}
---
+++ HomePage.LOcators.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Tema_28._10.PageObjects
+{
+    partial class HomePage
+    {
+        public IWebDriver _driver;
+
+        public HomePage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+        public IWebElement imageFrame => _driver.FindElement(By.XPath("//*[@id='Sva75c']"));
+        private IWebElement agreeButton => _driver.FindElement(By.XPath("//*[@id='introAgreeButton']/span/span"));
+        public IWebElement searchTextBox => _driver.FindElement(By.XPath("//input[@name='q']"));
+        private IWebElement imagesButton => _driver.FindElement(By.XPath("//*[@id='hdtb-msb-vis']/div[2]/a"));
+        private IWebElement firstImageFound => _driver.FindElement(By.XPath("//*[@id='islrg']/div[1]/div[1]/a[1]/div[1]/img"));
+       
+    }
+}
